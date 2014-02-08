609f242c1ebd60abbcb04f428dfa5cd853478585
Julian Hunt <Julian@Julians-MacBook-Pro.local>
2014-02-08 13:25:03 -0500

    Initial Commit

---
+++ Assignment 1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment 1
+//
+//  Created by Julian Hunt on 2/8/2014.
+//  Copyright (c) 2014 Julian Hunt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
